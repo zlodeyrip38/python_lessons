83fbd73bd24c6e4c85ebcd664bf3f09c3693b66a
lyubomyr83 <lyubomyr83@mail.ru>
2019-09-28 17:01:33 +0300

    Первый коммит

--- rodjer.py
+++ rodjer.py
@@ -2,18 +2,39 @@ from time import sleep
 from random import randint, choice
 
 
-print ('Привет меня завут Роджер. А тебя как ?')
+print ('Привет меня завут Роджер. А тебя как?')
 name = input()
+name = name.title()
 print('Приятно пазнакомиться, ' + name)
-sleep(2)
+sleep(1)
 print('Давай проверим твои знания в математике.')
-sleep(2)
+sleep(1)
 print('Ты готов? (да или нет)')
 
 ready = input()
+ready = ready.lower()
+
+while ready not in {'да', 'нет'}:
+    print('''Должно быть 'да' или 'нет'
+Введите заново''')
+    ready = input()
+    ready = ready.lower()
+
 if ready == 'да':
-    print('Сколько примеров ты готов решить')
-    col = int(input())
+    col = '' # количество примеров
+
+    while not col.isdigit():
+        print('Сколько примеров ты готов решить')
+        col = input()
+        while not col.isdigit():
+            print('Должна быть цифра')
+            col = input()
+        while col.isdigit():
+            col = int(col)
+            while col < 1:
+                print('Введите число больше 0')
+                col = int(input())
+
     print('До скольки будем считать? Например, до 100 ')
     max_answer = int(input())
     sleep(1)
@@ -32,3 +53,6 @@ if ready == 'да':
         sign = choice('+-')
 
         print(numder1,sign,numder2)
+else:
+    print('''Передумал? Хорошо , может какнибуть в следующий раз ...
+Пока!''')
